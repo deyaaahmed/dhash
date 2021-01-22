2c2036d26f31d671e8321a2a9ad69b716e56da42
7azabet <66400295+7azabet@users.noreply.github.com>
2021-01-22 10:35:14 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Waiting.."
+
+for (( i = 0; i < n; i++ )); do
+
+done
+sudo apt-get update -y ; sudo apt-get upgrade -y > /dev/null
+pip3 install -r requirements.txt > /dev/null
